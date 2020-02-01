74d1e49ec967c6dbcafe40b76ebfa657d5254a4d
schlenkibus <justus.scheil@parteimail.de>
2020-02-01 16:58:15 +0100

    wip

--- unity/ggj20/Assets/Scripts/AI/Wander.cs
+++ unity/ggj20/Assets/Scripts/AI/Wander.cs
@@ -21,33 +21,53 @@ public class Wander : MonoBehaviour
 		StartCoroutine(NewHeading());
 	}
  
+    GameObject getClosestIsland() {
+        GameObject ret = null;
+        float minDist = 9999999f;
+        GameObject[] islands = GameObject.FindGameObjectsWithTag("Floor"); 
+        foreach (GameObject island in islands)
+        {
+            float currentDistance = Vector3.Distance(island.transform.position, transform.position);
+            if(currentDistance < minDist) {
+                minDist = currentDistance;
+                ret = island;
+            }
+        }
+        return ret;
+    }
+
 	void FixedUpdate ()
 	{
         if(!m_enabled)
             return;
 
         RaycastHit hit;
+        RaycastHit hit2;
 
         var forward = transform.TransformDirection(Vector3.forward);
         var down = transform.TransformDirection(Vector3.down);
-        Vector3 direction = forward + down;
+        var down3 = transform.TransformDirection(Vector3.down * 3);
+        Vector3 directionFrontDown = forward + down;
+        Vector3 directionFrontDowner = forward + down3;
 
-        if (Physics.Raycast(transform.position, direction, out hit, 200.0f))
+        transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, targetRotation, Time.deltaTime * directionChangeInterval);
+
+        if (Physics.Raycast(transform.position, directionFrontDown, out hit, 50.0f) && Physics.Raycast(transform.position, directionFrontDown, out hit2, 50.0f))
         {
-            if(hit.collider.tag == "Floor") {
-                transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, targetRotation, Time.deltaTime * directionChangeInterval);
+            if(hit.collider.tag == "Floor" && hit2.collider.tag == "Floor") {
                 forward = transform.TransformDirection(Vector3.forward);
                 controller.SimpleMove(forward * speed);
                 return;
             }
         }
 
+        //Turn 180° and calculate new angle to approach
         transform.RotateAround(transform.position, transform.up, 180f);
         heading -= 180;
         if(heading < 0)
             heading = 360 - heading;
 
-        targetRotation = new Vector3(0, heading, 0);
+        NewHeadingRoutine();
 	}
  
 	IEnumerator NewHeading ()
--- unity/ggj20/Assets/Scripts/IslandController.cs
+++ unity/ggj20/Assets/Scripts/IslandController.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 public class IslandController : MonoBehaviour
 {
-
     HashSet<GameObject> m_currentIslandEntitys;
 
-    // Start is called before the first frame update
     void Start()
     {
         m_currentIslandEntitys = new HashSet<GameObject>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         
@@ -22,12 +19,10 @@ public class IslandController : MonoBehaviour
     public void onEntityJoinedIsland(GameObject entity)
     {
         m_currentIslandEntitys.Add(entity);
-        Debug.Log("Joined: " + m_currentIslandEntitys.Count);
     }
 
     public void onEntityLeftIsland(GameObject entity)
     {
         m_currentIslandEntitys.Remove(entity);
-        Debug.Log("Left: " + m_currentIslandEntitys.Count);
     }
 }
