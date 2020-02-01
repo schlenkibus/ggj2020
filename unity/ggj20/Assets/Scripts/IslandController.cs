169961caa79d7235cfa8931db116c78a32e270a5
schlenkibus <justus.scheil@parteimail.de>
2020-02-01 15:04:13 +0100

    started adding island model functionality

---
+++ unity/ggj20/Assets/Scripts/IslandController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IslandController : MonoBehaviour
+{
+
+    HashSet<GameObject> m_currentIslandEntitys;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_currentIslandEntitys = new HashSet<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onEntityJoinedIsland(GameObject entity)
+    {
+        m_currentIslandEntitys.Add(entity);
+        Debug.Log("Joined: " + m_currentIslandEntitys.Count);
+    }
+
+    public void onEntityLeftIsland(GameObject entity)
+    {
+        m_currentIslandEntitys.Remove(entity);
+        Debug.Log("Left: " + m_currentIslandEntitys.Count);
+    }
+}
