246b489241fdfad12f1ccc0cf2fa4d9786e733a8
Philipp H <hafellner.philipp@gmail.com>
2020-02-02 17:01:00 +0100

    finish

--- unity/ggj20/Assets/Scripts/Game/GameManager.cs
+++ unity/ggj20/Assets/Scripts/Game/GameManager.cs
@@ -24,31 +24,35 @@ public class GameManager : MonoBehaviour
     }
 
     void setupWorld1() {
+
+        //summer
         Dictionary<MetricType, float> factors = new Dictionary<MetricType, float>();
         List<Metric> goals = new List<Metric>();
         
-        goals.Add(new Metric(MetricType.Rat, 2));
+        goals.Add(new Metric(MetricType.Rat, 9));
         goals.Add(new Metric(MetricType.Fox, 3));
-        goals.Add(new Metric(MetricType.Penguin, 3));
+        goals.Add(new Metric(MetricType.Penguin, 1));
         
         factors.Add(MetricType.Rat, 1f);
-        factors.Add(MetricType.Fox, 1f);
-        factors.Add(MetricType.Penguin, 1f);
+        factors.Add(MetricType.Fox, 0.4f);
+        factors.Add(MetricType.Penguin, 0.1f);
         
         summerIsland.m_goal = new Goal(goals, factors);
     }
 
     void setupWorld2() {
+
+        //winter
         Dictionary<MetricType, float> factors = new Dictionary<MetricType, float>();
         List<Metric> goals = new List<Metric>();
 
-        goals.Add(new Metric(MetricType.Rat, 5));
+        goals.Add(new Metric(MetricType.Rat, 1));
         goals.Add(new Metric(MetricType.Fox, 5));
-        goals.Add(new Metric(MetricType.Penguin, 5));
+        goals.Add(new Metric(MetricType.Penguin, 8));
         
-        factors.Add(MetricType.Rat, 1f);
-        factors.Add(MetricType.Fox, 1f);
-        factors.Add(MetricType.Penguin, 1f);
+        factors.Add(MetricType.Rat, 0.2f);
+        factors.Add(MetricType.Fox, 0.5f);
+        factors.Add(MetricType.Penguin, 0.3f);
         
         winterIsland.m_goal = new Goal(goals, factors);
     }
